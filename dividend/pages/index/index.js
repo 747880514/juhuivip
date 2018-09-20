d21c32031ed9b4008d34bc2e76abfe841f049729
zhanghaian <2234151617@qq.com>
2018-09-20 18:18:18 +0800

    团队样式修改、

--- dividend/pages/index/index.js
+++ dividend/pages/index/index.js
@@ -7,13 +7,13 @@ e.requirejs("jquery"), Page({
     // 屠苏返回键
     return_return_top_tushu: function () {
 
-      wx.navigateBack({ changed: true });
+      // wx.navigateBack({ changed: true });
       // wx.reLanch({
       //   url: '/pages/diy/tushu/equities/equities'
       // })
-      // wx.switchTab({
-      //   url: '/pages/diy/tushu/equities/equities'
-      // })
+      wx.switchTab({
+        url: '/pages/diy/tushu/equities/equities'
+      })
       // wx.navigateTo({
       //   url: '/pages/diy/tushu/equities/equities'
       // })
